68da803b93b1172e7873c884f901ca39a649137f
Bart Pede <bart@mobicage.com>
2019-04-18 16:15:08 +0200

    Added MISSING.default

--- mcfw/consts.py
+++ mcfw/consts.py
@@ -21,6 +21,7 @@ import os
 
 
 class MissingClass(object):
+
     def __reduce__(self):
         # See pickle documentation:
         #
@@ -28,6 +29,9 @@ class MissingClass(object):
         # It should be the objects local name relative to its module; the pickle module searches the module
         # namespace to determine the objects module. This behaviour is typically useful for singletons.
         return 'MISSING'
+
+    def default(self, value, value_if_missing):
+        return value_if_missing if value is self else value
 
 
 MISSING = MissingClass()
